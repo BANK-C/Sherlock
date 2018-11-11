36a1063dbe887f6aed9c528e47eb0676e060ed04
nat <nl356@cornell.edu>
2018-11-11 00:10:51 -0500

    speaker

--- post_output.py
+++ post_output.py
@@ -4,7 +4,7 @@ import requests
   
 # defining the api-endpoint  
 # API_ENDPOINT = "http://[SoundTouch product LAN IP Address]:8090/speaker"
-API_ENDPOINT = "http://172.20.10.3:8090/speaker"  
+API_ENDPOINT = "http://172.20.10.7:8090/speaker"  
 
 # your API key here 
 API_KEY = "it5MqoTbZ3aZhZM16P6baHVHyBvA6kwouL51KkDWTq2s"
@@ -13,12 +13,7 @@ SERVICE = "bank-c"
 
   
 # data to be sent to api 
-data = 
-<play_info>
-  <app_key>API_KEY</app_key>
-  <url>URL</url>
-  <service>SERVICE</service>
-</play_info>
+data = "<play_info><app_key>API_KEY</app_key><url>URL</url><service>SERVICE</service></play_info>"
   
 # sending post request and saving response as response object 
 r = requests.post(url = API_ENDPOINT, data = data) 
