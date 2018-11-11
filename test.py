9e3cd1b565b21a9121edaf7f2e21d7a429f03ca1
Karen Fang <kf288@cornell.edu>
2018-11-10 22:36:01 -0500

    baby

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import time
 import uuid
 
 camera = cv2.VideoCapture(0)
+int1 = 0
 while(True):
     _, image = camera.read()
-    cv2.imwrite(str(uuid.uuid4())+'.png', image)
+    cv2.imwrite("test"+str(int1)+'.png', image)
     time.sleep(5)
--- woohoo.py
+++ woohoo.py
@@ -2,19 +2,33 @@ import base64
 import webbrowser
 import os
 import subprocess
+import json
 
 #Code below returns json
 #os.system('curl -X POST -u "apikey:it5MqoTbZ3aZhZM16P6baHVHyBvA6kwouL51KkDWTq2s" --form "images_file=@test.png" --form "classifier_ids=detect_faces" "https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces?version=2018-03-19"')
 #cmd = ['curl -X POST -u "apikey:it5MqoTbZ3aZhZM16P6baHVHyBvA6kwouL51KkDWTq2s" --form "images_file=@test.png" --form "classifier_ids=detect_faces" "https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces?version=2018-03-19"']
-str1 = 'curl -X POST -u "apikey:it5MqoTbZ3aZhZM16P6baHVHyBvA6kwouL51KkDWTq2s" --form "images_file=@test.png" --form "classifier_ids=detect_faces" "https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces?version=2018-03-19"'
+picture = "baby1.png"
+str1 = 'curl -X POST -u "apikey:it5MqoTbZ3aZhZM16P6baHVHyBvA6kwouL51KkDWTq2s" --form "images_file=@'+picture+'" --form "classifier_ids=detect_faces" "https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces?version=2018-03-19"'
 #cmd = ['curl', '-X', 'POST', '-u', '"apikey:it5MqoTbZ3aZhZM16P6baHVHyBvA6kwouL51KkDWTq2s"', '--form', '"images_file=@test.png"', '--form', '"classifier_ids=detect_faces"', '"https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces?version=2018-03-19"']
 cmd = str1.split(" ")
-print(cmd)
+#print(cmd)
 result = subprocess.run(cmd, stdout=subprocess.PIPE)
 output=subprocess.check_output(str1)
+o_s = output.decode('utf-8')
+
+json_acceptable_string = o_s.replace("'", "\"")
+d = json.loads(json_acceptable_string)
+
+age_json = d['images'][0]['faces'][0]['age']
+
+if age_json["min"] < 5:
+	output = "DON'T TOUCH THAT, you're way too young!"
+else:
+	output = "Welcome to your cabinet!"
+
 text_file = open("Output.txt", "w")
-text_file.write(output.decode())
+text_file.write(output)
 text_file.close()
-print(output)
+#print(d['images'][0]['faces'][0]['age'])
 #result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
 #print(result.stdout.decode('utf-8'))
