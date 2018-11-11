895260c91176cea15abc5f9338a597b59bed636b
Karen Fang <kf288@cornell.edu>
2018-11-11 11:31:03 -0500

    cropping

--- docs/test.py
+++ docs/test.py
@@ -7,8 +7,12 @@ camera = cv2.VideoCapture(0)
 int1 = 0
 while(True):
     _, image = camera.read()
-    #cropped = image[0:720, 440:840]
-    cropped=- image
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    crop_w = int(width/3)
+    crop_w2 = int(width* 2/3)
+    cropped = image[0:height, crop_w:crop_w2]
     cv2.imwrite("test"+'.png', cropped)
     time.sleep(5)
     os.system('python woohoo.py')
